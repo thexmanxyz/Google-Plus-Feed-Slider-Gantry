f4e02846ed58ca32b20e38f536eae768175a3f8b
thex <andreas.kar@gmx.at>
2018-05-30 20:34:48 +0200

    legacy support, whitespace cleanup

--- src/js/gplus-feed-slider.js
+++ src/js/gplus-feed-slider.js
@@ -14,41 +14,41 @@
 *******************************************************/
 
 function GPlusFeedSilder(){
-    
+
     //runtime information
     this.currentSlide = 0;
     this.delayedAuto = false;
-    
+
     //configuration of slider
     this.cfg = {container_id: "1",
                 api_key: "",
                 profile: "+LarryPage",
                 collection: "public",
                 maxResults: 20,
-                 
+
                 link_enable: true,
                 link_title: "Link to Google+ Post",
                 filter_empty: true,
                 empty_title: "go to post",
-                
+
                 display_name: false,
                 plus_one: true,
                 slide_count: false,
-                 
+
                 slider_auto: true,
                 slider_delay: 5000,
                 slider_mode: 1,
-                 
+
                 image_enable: true,
                 image_design: 2,
                 image_size: 25,
                 image_alt: "Google Plus Profile",
-                 
+
                 date_mode: 3,
                 date_format: 2,
                 date_delimiter: '/',
                 time_delimiter: ' -'};
-    
+
     //check if should be slided and clear old state
     this.isSlidable = function (gfs, slides, auto){
         if(auto && gfs.delayedAuto) {
@@ -62,7 +62,7 @@ function GPlusFeedSilder(){
         }
         return true;
     };
-    
+
     //scrolling right
     this.slideRight = function slideRight(gfs, slides, auto) {
         return function(){
@@ -71,7 +71,7 @@ function GPlusFeedSilder(){
             slides[gfs.currentSlide].style.display = "inherit";
         };
     };
-    
+
     //scrolling left
     this.slideLeft = function slideLeft(gfs, slides, auto) {
         return function(){
@@ -80,15 +80,14 @@ function GPlusFeedSilder(){
             slides[gfs.currentSlide].style.display = "inherit";
         };
     };
-    
-    
+
     //date by mode
     this.slideDate = function(slide){
         var date = new Date(Date.parse(slide.published));
         var prepare = function(val){ return (val < 10 ? "0" + val : val); };
         var del = this.cfg.date_delimiter;
         var dateTime = "";
-        
+
         if(this.cfg.date_mode == 1 || this.cfg.date_mode == 3){
             dateTime += "&nbsp;";
             switch(this.cfg.date_format){
@@ -105,13 +104,13 @@ function GPlusFeedSilder(){
                     dateTime += date.getFullYear() + del + prepare(date.getMonth()) + del + prepare(date.getDate());
             }
         }
-        
+
         if(this.cfg.date_mode == 2 || this.cfg.date_mode == 3){
             dateTime += this.cfg.time_delimiter + " " + prepare(date.getHours()) + ":" + prepare(date.getMinutes());  
         }
         return dateTime;
     };
-    
+
     //build slide
     this.buildSlide = function(gfs, slide, state, count){
         var cls = {on: "slide-on",
@@ -122,30 +121,30 @@ function GPlusFeedSilder(){
                      pluses: "slide-plus-one",
                      img_cont: "slide-image-container",
                      img: "slide-person-image slide-image-mode-" + gfs.cfg.image_design};
-        
+
         var imgUrl = slide.actor.image.url;
         var slideText;
         var html;
-        
+
         slideText = (gfs.cfg.slide_count ? count + ". | " : "") + (!slide.title ? gfs.cfg.empty_title : slide.title);
         imgUrl = ((gfs.cfg.image_size != 50) ? imgUrl.replace("sz=50", "sz=25") : imgUrl);
-        
+
         html = "<div class=\"" + cls.element + "" + (state ? " " + cls.on : " " + cls.off) + "\"" + ">";
         html += ((gfs.cfg.link_enable) ? "<a href=\"" + slide.url + "\" title=\"" + gfs.cfg.link_title + "\">" + slideText + "</a>" : slideText);
         html += "<span class=\"" + cls.date + "\">&nbsp;" + gfs.slideDate(slide)  + "</span>";
         html += "<span class=\"" + cls.img_cont + "\">";
-        
+
         if(gfs.cfg.image_enable){
             html += "<img class=\"" + cls.img + "\" src=\"" + imgUrl + "\" ";
             html += "width=\"" + gfs.cfg.image_size + "\" ";
             html += "height=\"" + gfs.cfg.image_size + "\" ";
             html += "alt=\"" + gfs.cfg.image_alt + "\">";
         }
-        
+
         html += "<span class=\"" + cls.name + "\">" + ((gfs.cfg.display_name) ? "&nbsp;" + slide.actor.displayName : "");
         html += "<span class=\"" + cls.pluses + "\">" + ((gfs.cfg.plus_one) ? "&nbsp;+" + slide.object.plusoners.totalItems : "");
         html += "</span></span></div>";
-        
+
         return html;
     };
     
@@ -157,7 +156,7 @@ function GPlusFeedSilder(){
         url += "&maxResults=" + ((this.cfg.maxResults) ? this.cfg.maxResults : 20);
         return url;
     };
-    
+
     //fetch feed
     this.addLoadFeedEvent = function(){
         var xhttp = new XMLHttpRequest();  
@@ -166,23 +165,23 @@ function GPlusFeedSilder(){
         xhttp.onload = this.getFeedLoadEvent(this);
         xhttp.send(null);         
     };
-    
+
     this.getFeedLoadEvent = function(gfs) {
         return function(){
-            
+
             //selectors
             var sel = {slider: ".g-google-plus-slider.slider-" + gfs.cfg.container_id,
                        content: ".content-container",
                        element: ".slide-element",
                        left: ".scroll-left",
                        right: ".scroll-right"};
-            
+
             var json = JSON.parse(this.responseText);
             var $slider = jQuery(sel.slider);
             var slides = [];
             var html = "";
             var count = 1;
-            
+
             //build slides
             for(var i=0; i < Object.keys(json.items).length; i++){
                 if((!gfs.cfg.filter_empty && json.items[i].title != "" && json.items[i].title.length > 0) || gfs.cfg.filter_empty){
@@ -194,13 +193,13 @@ function GPlusFeedSilder(){
             //output slides
             $slider.find(sel.content).html(html);
             slides = jQuery(sel.slider + " " + sel.content + " > " + sel.element)
-            
+
             //repeating event (autoscroll)
             if(gfs.cfg.slider_auto){
                 var slideDir = (gfs.cfg.slider_mode == 1) ? gfs.slideRight(gfs, slides, true) : gfs.slideLeft(gfs, slides, true);
                 setInterval(slideDir, gfs.cfg.slider_delay);
             }
-            
+
             //button click events
             $slider.find(sel.right).on('click', gfs.slideRight(gfs, slides, false));
             $slider.find(sel.left).on('click', gfs.slideLeft(gfs, slides, false));
